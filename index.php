5555a54003f1232d30b81cec6545cec4d433bd68
Rohit Kumar Kushwaha <143890552+Tylerdude-877@users.noreply.github.com>
2025-04-26 13:37:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php 
+session_start();
+$logged = false;
+if (isset($_SESSION['user_id']) && isset($_SESSION['username'])) {
+	 $logged = true;
+	 $user_id = $_SESSION['user_id'];
+    }
+
+
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Home Page</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<?php 
+        include 'inc/NavBar.php';
+	 ?>
+     <div class="main">
+  <div class="intro">
+  <h1>Welcome to My World</h1>
+  <p>
+    Step into a space where stories unfold through every plate tasted, every page turned, and every path explored.
+    This blog is my journal — a blend of travels, food journeys, favorite books, thoughts, and little adventures that make life beautiful.
+  </p>
+  <p>
+    Whether you're here to find your next read, your next trip, or just some peace in words — I'm glad you made it.  
+    Let’s wander, taste, read, and grow together.
+  </p>
+
+  </div>
+     </div>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
